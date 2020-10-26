13fd10ee2545ffce1e4e28b0df7dc17b68077b77
K.K <gokennsirou@yahoo.co.jp>
2020-10-27 06:58:45 +0900

    Add Card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>
-    <slot></slot>
-    {{ $slots }}
+  <div class="
+    rounded-sm
+    shadow
+    bg-white
+    text-current
+  ">
+    <div class="py-4 px-5 border-solid border-b border-gray-200" v-if="$slots.header || header">
+      <slot name="header">{{ header }}</slot>
+    </div>
+    <div class="p-5">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 export default defineComponent({
-  
+  props: {
+    header: String
+  }
 })
 </script>
--- src/views/Cards.vue
+++ src/views/Cards.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <Card>スロット</Card>
+    <Card class="w-4/6 mx-auto">
+      <template #header>ログイン</template>
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem a libero ipsum dolorum dolor voluptatum adipisci, nisi, possimus amet, eos debitis sapiente culpa incidunt necessitatibus! Nihil doloremque nesciunt quibusdam tempora!</p>
+      </div>
+    </Card>
   </div>
 </template>
 <script lang="ts">
